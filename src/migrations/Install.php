37238099a9a80f001094031666940f41c4c654e2
Maxime Lafrenière <ml@mambomambo.ca>
2026-04-24 10:46:57 -0400

    Refactor Install migration to check for existing tables before creation. Added 'permanentBlockAfterAutomaticBlocks' field to fort_runtime table. Ensured indexes are created only if tables are newly created.

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -9,88 +9,98 @@ class Install extends Migration
 {
     public function safeUp(): bool
     {
-        $this->createTable('{{%fort_security_events}}', [
-            'id' => $this->primaryKey(),
-            'siteId' => $this->integer()->null(),
-            'type' => $this->string(64)->notNull(),
-            'clientIp' => $this->string(45)->notNull(),
-            'requestPath' => $this->string(1024)->null(),
-            'meta' => $this->text()->null(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'dateUpdated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%fort_security_events}}')) {
+            $this->createTable('{{%fort_security_events}}', [
+                'id' => $this->primaryKey(),
+                'siteId' => $this->integer()->null(),
+                'type' => $this->string(64)->notNull(),
+                'clientIp' => $this->string(45)->notNull(),
+                'requestPath' => $this->string(1024)->null(),
+                'meta' => $this->text()->null(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
 
-        $this->createIndex(null, '{{%fort_security_events}}', ['type', 'dateCreated']);
-        $this->createIndex(null, '{{%fort_security_events}}', ['clientIp', 'dateCreated']);
-        $this->createIndex(null, '{{%fort_security_events}}', ['dateCreated']);
+            $this->createIndex(null, '{{%fort_security_events}}', ['type', 'dateCreated']);
+            $this->createIndex(null, '{{%fort_security_events}}', ['clientIp', 'dateCreated']);
+            $this->createIndex(null, '{{%fort_security_events}}', ['dateCreated']);
+        }
 
-        $this->createTable('{{%fort_runtime}}', [
-            'id' => $this->primaryKey(),
-            'defaultBlockDurationMinutes' => $this->integer()->null(),
-            'failedLoginThreshold' => $this->integer()->null(),
-            'failedLoginWindowMinutes' => $this->integer()->null(),
-            'maxRequestsPerIpPerMinute' => $this->integer()->null(),
-            'httpRateLimitAlertsBeforeBlock' => $this->integer()->null(),
-            'httpRateLimitAlertWindowMinutes' => $this->integer()->null(),
-            'lastDailyDigestSentAt' => $this->dateTime()->null(),
-            'lastWeeklyDigestSentAt' => $this->dateTime()->null(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'dateUpdated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%fort_runtime}}')) {
+            $this->createTable('{{%fort_runtime}}', [
+                'id' => $this->primaryKey(),
+                'defaultBlockDurationMinutes' => $this->integer()->null(),
+                'permanentBlockAfterAutomaticBlocks' => $this->integer()->null(),
+                'failedLoginThreshold' => $this->integer()->null(),
+                'failedLoginWindowMinutes' => $this->integer()->null(),
+                'maxRequestsPerIpPerMinute' => $this->integer()->null(),
+                'httpRateLimitAlertsBeforeBlock' => $this->integer()->null(),
+                'httpRateLimitAlertWindowMinutes' => $this->integer()->null(),
+                'lastDailyDigestSentAt' => $this->dateTime()->null(),
+                'lastWeeklyDigestSentAt' => $this->dateTime()->null(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
 
-        $now = (new \DateTimeImmutable())->format('Y-m-d H:i:s');
-        $this->insert('{{%fort_runtime}}', [
-            'id' => 1,
-            'defaultBlockDurationMinutes' => null,
-            'failedLoginThreshold' => null,
-            'failedLoginWindowMinutes' => null,
-            'maxRequestsPerIpPerMinute' => null,
-            'httpRateLimitAlertsBeforeBlock' => null,
-            'httpRateLimitAlertWindowMinutes' => null,
-            'lastDailyDigestSentAt' => null,
-            'lastWeeklyDigestSentAt' => null,
-            'dateCreated' => $now,
-            'dateUpdated' => $now,
-            'uid' => StringHelper::UUID(),
-        ]);
+            $now = (new \DateTimeImmutable())->format('Y-m-d H:i:s');
+            $this->insert('{{%fort_runtime}}', [
+                'id' => 1,
+                'defaultBlockDurationMinutes' => null,
+                'permanentBlockAfterAutomaticBlocks' => null,
+                'failedLoginThreshold' => null,
+                'failedLoginWindowMinutes' => null,
+                'maxRequestsPerIpPerMinute' => null,
+                'httpRateLimitAlertsBeforeBlock' => null,
+                'httpRateLimitAlertWindowMinutes' => null,
+                'lastDailyDigestSentAt' => null,
+                'lastWeeklyDigestSentAt' => null,
+                'dateCreated' => $now,
+                'dateUpdated' => $now,
+                'uid' => StringHelper::UUID(),
+            ]);
+        }
 
-        $this->createTable('{{%fort_blocked_ips}}', [
-            'id' => $this->primaryKey(),
-            'clientIp' => $this->string(45)->notNull(),
-            'blocked' => $this->boolean()->notNull()->defaultValue(true),
-            'blockedUntil' => $this->dateTime()->null(),
-            'blockCount' => $this->integer()->notNull()->unsigned()->defaultValue(0),
-            'manual' => $this->boolean()->notNull()->defaultValue(false),
-            'notes' => $this->text()->null(),
-            'lastReason' => $this->string(32)->null(),
-            'lastBlockedAt' => $this->dateTime()->null(),
-            'isPermanent' => $this->boolean()->notNull()->defaultValue(false),
-            'nextPermanentThreshold' => $this->integer()->null(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'dateUpdated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%fort_blocked_ips}}')) {
+            $this->createTable('{{%fort_blocked_ips}}', [
+                'id' => $this->primaryKey(),
+                'clientIp' => $this->string(45)->notNull(),
+                'blocked' => $this->boolean()->notNull()->defaultValue(true),
+                'blockedUntil' => $this->dateTime()->null(),
+                'blockCount' => $this->integer()->notNull()->unsigned()->defaultValue(0),
+                'manual' => $this->boolean()->notNull()->defaultValue(false),
+                'notes' => $this->text()->null(),
+                'lastReason' => $this->string(32)->null(),
+                'lastBlockedAt' => $this->dateTime()->null(),
+                'isPermanent' => $this->boolean()->notNull()->defaultValue(false),
+                'nextPermanentThreshold' => $this->integer()->null(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
 
-        $this->createIndex(null, '{{%fort_blocked_ips}}', ['clientIp'], true);
-        $this->createIndex(null, '{{%fort_blocked_ips}}', ['blocked', 'blockedUntil']);
-        $this->createIndex(null, '{{%fort_blocked_ips}}', ['lastBlockedAt']);
+            $this->createIndex(null, '{{%fort_blocked_ips}}', ['clientIp'], true);
+            $this->createIndex(null, '{{%fort_blocked_ips}}', ['blocked', 'blockedUntil']);
+            $this->createIndex(null, '{{%fort_blocked_ips}}', ['lastBlockedAt']);
+        }
 
-        $this->createTable('{{%fort_alerts}}', [
-            'id' => $this->primaryKey(),
-            'siteId' => $this->integer()->null(),
-            'alertType' => $this->string(64)->notNull(),
-            'clientIp' => $this->string(45)->notNull(),
-            'requestPath' => $this->string(1024)->null(),
-            'payload' => $this->text()->null(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%fort_alerts}}')) {
+            $this->createTable('{{%fort_alerts}}', [
+                'id' => $this->primaryKey(),
+                'siteId' => $this->integer()->null(),
+                'alertType' => $this->string(64)->notNull(),
+                'clientIp' => $this->string(45)->notNull(),
+                'requestPath' => $this->string(1024)->null(),
+                'payload' => $this->text()->null(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
 
-        $this->createIndex(null, '{{%fort_alerts}}', ['alertType', 'dateCreated']);
-        $this->createIndex(null, '{{%fort_alerts}}', ['clientIp', 'dateCreated']);
-        $this->createIndex(null, '{{%fort_alerts}}', ['dateCreated']);
+            $this->createIndex(null, '{{%fort_alerts}}', ['alertType', 'dateCreated']);
+            $this->createIndex(null, '{{%fort_alerts}}', ['clientIp', 'dateCreated']);
+            $this->createIndex(null, '{{%fort_alerts}}', ['dateCreated']);
+        }
 
         return true;
     }
